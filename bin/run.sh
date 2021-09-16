a4f959f7248f5aca8795edf71f36e04c51de2130
Bruno Raoult <braoult@gmail.com>
2021-09-16 12:39:16 +0200

    Reflect bats changes (https://github.com/exercism/bash/pull/541)

--- bin/run.sh
+++ bin/run.sh
@@ -66,13 +66,16 @@ run_tests() {
     # Run tests and pipe output to results.out file.
 
     local slug="$1"
-    local test_file=${slug//-/_}_test.sh
+    local test_file=${slug//-/_}.bats
     local solution_dir="$2"
     local output_file="$3"
 
     echo "Running tests."
 
     cd "$solution_dir"
+
+    # test scripts may be (old) xxxx_test.sh
+    [[ -f "$test_file" ]] || test_file=${slug//-/_}_test.sh
 
     sed -i 's/load bats-extra.bash/load bats-extra/' "$test_file" || true
 
